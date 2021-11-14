c8b37073663744593aa6dcf7f7ca671fd28731f9
unknown <zubairjava123@gmail.com>
2021-11-14 18:20:43 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,224 @@
+// alert("HELLO CLASS...")
+
+
+///variables///
+
+
+
+// var name = "Muhammad Zubair";
+
+// var age = 20;
+
+
+// alert(name)
+
+
+///datatypes
+// string
+// Number
+// boolean 
+// undefined
+// null
+
+
+
+
+// var Name = "Jaffar"
+
+// var firstName = "Jaffar"
+// var last_namE = "aman"
+
+// alert(firstName + last_namE)
+
+
+////MATH EXPRESSTION///
+// +
+// -
+// *
+// /
+// %
+
+// var num1 = 300;
+// var num2 = 500;
+
+// var add = num1 + num2
+
+// alert(add)
+
+
+// var result = (2-4) * 10 + 20;
+//               -2 *10 + 20
+//               -20 +20
+//               0
+
+// alert(result)
+
+
+// alert(3)
+// alert(1)
+// alert(2)
+
+
+// var a = "50" ///string
+// var b = 20 ////number
+
+
+// document.write(a + b)
+
+
+
+// var name = "Jaffar"
+// var name2 = "aman"
+
+// document.write(name + name2)
+
+// var num1 = 30
+// var num2 = 50
+
+// document.write(num1 + num2)
+
+
+// var num = 10;
+
+// var update_num = num++ //POST
+// var update_num = ++num //PRE
+
+// var num2 = num
+
+
+// alert("update_num" + update_num);
+
+// alert("num2" + num2)
+
+// var num = 20;
+
+// var update_num = --num //POST
+
+// var num2 = num
+
+
+// alert("update_num" + update_num);
+
+// alert("num2" + num2)
+
+
+
+
+// var a = 20;
+
+// var result = a++  +   a++ - a  + ++a - a--
+//              20   +   21  - 22 + 23  - 23
+//                      19
+
+
+
+// alert(result)
+
+
+
+// var a = 15;
+
+// var result = a + a++ - a++ + a  - --a + a-- + a
+//         //    =15 + 15 - 16  +  17 - 16  + 16 + 15
+
+
+// alert(result)
+
+
+
+// var a = 16;
+// var result = a++ + a++ + a - a - --a - --a + ++a
+//             //   + 17 
+
+// alert(result)
+
+
+
+        ///user sy input///
+
+        //built in method hai user sy value ky liye///
+
+// var userName  = prompt("User Name")
+
+// document.write(userName)
+
+
+// var firstName = prompt("First Name") 
+// var lastName =  prompt("Last Name")
+
+// document.write(firstName + " " + lastName)
+
+
+// var name = prompt("enter your name");
+// var age = prompt("enter age")
+// var number = prompt("phone number")
+// var address = prompt("address")
+
+// document.write("USER NAME: "  + name )
+// document.write("<br> age: " + age )
+// document.write("<br> number: " + number )
+// document.write("<br> address: " + address )
+
+
+
+
+    ////
+    // var ?name
+    // var name = 
+
+    // full name
+    // school name
+    // class 
+    // eng 
+    // urdu
+    // isl
+    // Math
+    // science
+    // obtained marks  
+    // total mark   
+    // percentage   (obt / total) * 100
+
+//     var name = prompt("enter your name");
+//     var school = prompt("enter school name")
+//     var className = prompt("enter class")
+//     var eng = prompt("eng")
+//     var urdu = prompt("urdu")
+//     var isl = prompt("isl")
+//     var math = prompt("math")
+//     var science = prompt("science")
+//     var obtainedMarks = prompt("obtainedMarks")
+//     var totalMarks = prompt("totalMarks")
+//     var percentage = prompt(" percentage")
+
+    
+//     document.write("USER NAME: "  + name )
+//     document.write("<br> school: " + school )
+//     document.write("<br> classn Name: " + className )
+//     document.write("<br> eng: " + eng )
+//     document.write("<br> urdu: " + urdu )
+//     document.write("<br> isl: " + isl )
+//     document.write("<br> math: " + math )
+//     document.write("<br> science: " + science )
+//     document.write("<br> obtainedMarks: " + obtainedMarks )
+//     document.write("<br> totalMarks: " + totalMarks )
+//     document.write("<br> percentage: " + percentage )
+
+
+// var age = 18 ;
+// if(age > 20) {
+//     alert("Your is Greater")
+// }
+// else{
+//     alert("Your age is less")
+// }
+
+// var age =+prompt("Enter Your age")
+// if(age == 20)  {
+//     alert("Your age is equal")
+// }
+// else if(age < 20) {
+//     alert("Your age is less")
+// }
+// else{
+//     alert("Your age is more")
+// }
